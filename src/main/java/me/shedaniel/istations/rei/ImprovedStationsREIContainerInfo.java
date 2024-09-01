e56fa5cdfb9d753f33f2e7c51cdd2b57372f08eb
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-01 14:09:02 +0900

    SuppressWarnings removal

--- src/main/java/me/shedaniel/istations/rei/ImprovedStationsREIContainerInfo.java
+++ src/main/java/me/shedaniel/istations/rei/ImprovedStationsREIContainerInfo.java
@@ -16,6 +16,7 @@ import me.shedaniel.rei.plugin.common.displays.crafting.DefaultCraftingDisplay;
 import net.minecraft.world.entity.player.StackedContents;
 import net.minecraft.world.item.ItemStack;
 
+@SuppressWarnings("removal")
 public class ImprovedStationsREIContainerInfo implements REIServerPlugin {
     @Override
     public void registerMenuInfo(MenuInfoRegistry registry) {
