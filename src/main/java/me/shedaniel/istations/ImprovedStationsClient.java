22058b053a744be529a355671a7c87e994287d47
PTOM76 <ptms76@outlook.jp>
2025-08-16 22:04:59 +0900

    use New BlockRenderLayerMap API

--- src/main/java/me/shedaniel/istations/ImprovedStationsClient.java
+++ src/main/java/me/shedaniel/istations/ImprovedStationsClient.java
@@ -10,17 +10,17 @@ import me.shedaniel.istations.containers.CraftingStationScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 
 @Environment(EnvType.CLIENT)
 public class ImprovedStationsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         MenuScreens.register(ImprovedStations.CRAFTING_STATION_TYPE, CraftingStationScreen::new);
-        BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.cutout(), ImprovedStations.CRAFTING_STATION, ImprovedStations.CRAFTING_STATION_SLAB);
+        BlockRenderLayerMap.putBlocks(ChunkSectionLayer.CUTOUT, ImprovedStations.CRAFTING_STATION, ImprovedStations.CRAFTING_STATION_SLAB);
         BlockEntityRenderers.register(ImprovedStations.CRAFTING_STATION_BLOCK_ENTITY, CraftingStationBlockEntityRenderer::new);
     }
 }
